549b31dc9e38d4caf0d30457e698c14c79e23b06
WvDeldenHR <wvdelden99@outlook.com>
2024-06-04 13:40:29 +0200

    Updated packages

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+      <View style={styles.container}>
+        <Text className="">Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
