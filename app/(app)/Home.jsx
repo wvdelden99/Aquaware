437a150a5b4e2ec3a4b1b9502c50d3c9826ee00c
OzzyOs <Joshahursepuny@gmail.com>
2024-06-11 14:38:00 +0200

    feature/meting_inname :
    add:
    
    vocht inname functie in `Home`, uit `itemData` gehaald.

--- app/(app)/Home.jsx
+++ app/(app)/Home.jsx
@@ -1,15 +1,49 @@
-import { Image, Text, TouchableOpacity, View } from 'react-native';
+import {Button, Image, Text, TextInput, TouchableOpacity, View} from 'react-native';
 // Components
 import { LayoutBasic } from '../../components/layout/_layoutBasic';
-import { ItemData } from '../../components/content/item/ItemData';
 import { ItemMeting } from '../../components/content/item/ItemMeting';
-import { color } from '../../assets/styles/Styles';
+import { color } from '../../assets/styles/Styles';``
+import {useState} from "react";
+import * as Progress from 'react-native-progress';
+
 
 export function Home() {
+    const [consumed,setConsumed] = useState(0)      // Consumed state handler
+    const [inputValue, setInputValue] = useState('') // Input state handler
+
+    const variableCounter = () => (
+        parseInt(inputValue, 10) || 0                   // Variable uses value from input
+    )
+    // Will parse strings into an integer and default to 0
+    const increase = () => (
+        (
+            setConsumed(consumed + (variableCounter())
+            )
+        )
+    )
+    // Will change the Consumed state value
+    // By incrementing it with the value of variableCounter
+
+    const reset = () => (
+        setConsumed(0)
+    )                                                   // Will reset the Consumed state to 0
+
+
     return (
         <LayoutBasic>
-            <ItemData />
 
+            <View className="rounded-md w-full bg-gray-300">
+                <Progress.Bar className={'-rotate-90'} style={{ alignSelf: 'center', borderRadius: 10 }} progress={consumed /1500} width={250} height={300}/>
+
+                <Text className='self-center font-bold text-lg'>{consumed} / 1500</Text>
+
+                <TextInput onChangeText={setInputValue} className='border-2 h-12 w-24 self-center rounded-2xl'/>
+
+                <Button title={'Submit data'} onPress={increase} />
+                <Button title={'Reset'} onPress={reset} />
+            </View>
+
+            <Button title={'test'} onPress={()=>console.log({consumed})} />
             <View className="my-2 p-3 rounded-md bg-primary">
                 <View className="">
                     <Text className="text-base font-semibold text-white">Recente vochtinnames</Text>
