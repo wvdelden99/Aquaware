e213b8541e435e2d91f94150ef39c0b5287647ea
OzzyOs <Joshahursepuny@gmail.com>
2024-06-11 13:50:09 +0200

    Issue 'measure_progress' :
    Add :
    Progress Bar (works correctly)
    Added comments and removed unused import.
    
    Updated Packages

--- components/content/item/ItemData.jsx
+++ components/content/item/ItemData.jsx
@@ -1,6 +1,7 @@
-import {Button, Text, TextInput, View} from 'react-native';
+import {Alert, Button, Text, TextInput, View} from 'react-native';
 import {useState} from "react";
 import * as Progress from 'react-native-progress';
+import {ItemMeting} from "./ItemMeting";
 
 
 export function ItemData() {
@@ -9,19 +10,26 @@ export function ItemData() {
     const [inputValue, setInputValue] = useState('') // Input state handler
 
     const variableCounter = () => (
-        parseInt(inputValue, 10) || 0                       // Variable uses value from input
-    )                                                       // Will parse strings into an integer and default to 0
+        parseInt(inputValue, 10) || 0                   // Variable uses value from input
+    )
+                                                        // Will parse strings into an integer and default to 0
     const increase = () => (
-        setConsumed(consumed + (variableCounter()))         // Will change the Consumed state value
-    )                                                       // By incrementing it with the value of variableCounter
+         (
+           setConsumed(consumed + (variableCounter())
+           )
+       )
+    )
+                                                        // Will change the Consumed state value
+                                                        // By incrementing it with the value of variableCounter
 
-    const reset = () => {                             // Will reset the Consumed state to 0
-        return (
-            setConsumed(0)
-        )
-    }
+    const reset = () => (
+        setConsumed(0)
+    )                                                   // Will reset the Consumed state to 0
+
+
 
     return (
+
         <View className="rounded-md w-full bg-gray-300">
 
             <Progress.Bar style={{transform:[{rotate: '270deg'}], alignSelf: 'center', borderRadius: 10}} progress={consumed /1500} width={250} height={300}/>
@@ -33,5 +41,6 @@ export function ItemData() {
                     <Button title={'Submit data'} onPress={increase} />
                     <Button title={'Reset'} onPress={reset} />
         </View>
+
     )
 }
